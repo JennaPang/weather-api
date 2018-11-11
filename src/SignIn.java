65f162eacaca3546d05a7c4df38712f04ec227a6
unknown <[wndud990615@naver.com]>
2018-11-11 10:06:36 +0900

    회원가입 정보를 텍스트 파일에 넣기

--- src/SignIn.java
+++ src/SignIn.java
@@ -1,6 +1,6 @@
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.*;
+import java.io.*;
 
 import javax.swing.*;
 
@@ -17,46 +17,57 @@ public class SignIn extends JFrame {
 		
 		JLabel id = new JLabel("Id:");
 		id.setFont(new Font("HY그래픽M", Font.PLAIN, 13));
-		id.setBounds(92, 92, 17, 16);
+		id.setBounds(105, 92, 17, 16);
 		getContentPane().add(id);
 		
 		JLabel pswd = new JLabel("Password:");
 		pswd.setFont(new Font("HY그래픽M", Font.PLAIN, 13));
-		pswd.setBounds(45, 123, 64, 16);
+		pswd.setBounds(58, 123, 64, 16);
 		getContentPane().add(pswd);
 		
-		JLabel cp = new JLabel("Check password:");
-		cp.setFont(new Font("HY그래픽M", Font.PLAIN, 11));
-		cp.setBounds(20, 153, 89, 14);
-		getContentPane().add(cp);
+		JLabel cpswd = new JLabel("Check password:");
+		cpswd.setFont(new Font("HY그래픽M", Font.PLAIN, 11));
+		cpswd.setBounds(33, 153, 89, 14);
+		getContentPane().add(cpswd);
 		
 		JTextField idIn= new JTextField();
-		idIn.setBounds(121, 90, 184, 21);
+		idIn.setBounds(134, 90, 184, 21);
 		getContentPane().add(idIn);
 		idIn.setColumns(10);
 		
 		JPasswordField pswdIn = new JPasswordField();
-		pswdIn.setBounds(119, 149, 184, 21);
+		pswdIn.setBounds(132, 149, 184, 21);
 		getContentPane().add(pswdIn);
 		pswdIn.setColumns(10);
 		
 		JPasswordField checkPswd = new JPasswordField();
-		checkPswd.setBounds(119, 121, 184, 21);
+		checkPswd.setBounds(132, 121, 184, 21);
 		getContentPane().add(checkPswd);
 		checkPswd.setColumns(10);
 		
-		JButton log = new JButton("가입!");
-		log.setFont(new Font("굴림", Font.BOLD, 16));
-		log.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		log.setBackground(Color.WHITE);
-		log.setBounds(130, 185, 143, 35);
-		getContentPane().add(log);
-		
-		
-		
+		JButton sign = new JButton("가입!");
+		sign.setFont(new Font("굴림", Font.BOLD, 16));
+		sign.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+        		String i = idIn.getText();
+        		String p = new String(pswdIn.getPassword());
+        		String cp = new String(checkPswd.getPassword());
+                String fileName = "C:\\Users\\wndud\\eclipse-workspace\\IndividualProject\\res\\accounts.txt" ; 
+                try{             
+                    BufferedWriter fw = new BufferedWriter(new FileWriter(fileName, true));
+                    fw.write(i+"/"+cp+"/");
+                    fw.flush();
+                    fw.close();
+                }catch(Exception ex){
+                    ex.printStackTrace();
+                }
+            }
+        });
+		sign.setBackground(Color.WHITE);
+		sign.setBounds(130, 185, 143, 35);
+		getContentPane().add(sign);
+				
 		setTitle("회원가입");
 		setSize(388,304);
 		setResizable(false);

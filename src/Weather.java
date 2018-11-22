a9205f7f1ff742394fc097d8f2697cb48d35e8cd
unknown <[wndud990615@naver.com]>
2018-11-22 23:44:00 +0900

    결과창 디자인 1: 패널 만들기/seperator 삼입

--- src/Weather.java
+++ src/Weather.java
@@ -1,46 +1,60 @@
-import java.awt.EventQueue;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import java.awt.BorderLayout;
-
-public class Weather {
+public class Weather extends JFrame{
 
 	private JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Weather window = new Weather();
-					window.frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		Weather window = new Weather();
 	}
 
-	/**
-	 * Create the application.
-	 */
 	public Weather() {
-		initialize();
-	}
-
-	/**
-	 * Initialize the contents of the frame.
-	 */
-	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 350, 416);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setTitle("오늘의 날씨");
+		getContentPane().setLayout(null);
 		
-		JLabel lblWeather = new JLabel("Weather");
-		frame.getContentPane().add(lblWeather, BorderLayout.CENTER);
+		JPanel p1 = new JPanel();
+		p1.setBounds(0, 0, 338, 154);
+		getContentPane().add(p1);
+		p1.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lb1 = new JLabel("No.1");
+		lb1.setFont(new Font("함초롬돋움", Font.PLAIN, 27));
+		p1.add(lb1, BorderLayout.NORTH);
+		
+		JLabel lb101 = new JLabel("No.1 info");
+		lb101.setForeground(Color.ORANGE);
+		lb101.setHorizontalAlignment(SwingConstants.CENTER);
+		lb101.setFont(new Font("함초롬돋움", Font.BOLD, 40));
+		p1.add(lb101, BorderLayout.CENTER);
+		
+		JSeparator separator = new JSeparator();
+		p1.add(separator, BorderLayout.SOUTH);
+		
+		JPanel p2 = new JPanel();
+		p2.setBounds(0, 153, 162, 228);
+		getContentPane().add(p2);
+		p2.setLayout(new BorderLayout(0, 0));
+		
+		JSeparator separator_1 = new JSeparator();
+		separator_1.setOrientation(SwingConstants.VERTICAL);
+		p2.add(separator_1, BorderLayout.EAST);
+		
+		JPanel p3 = new JPanel();
+		p3.setBounds(162, 153, 176, 140);
+		getContentPane().add(p3);
+		p3.setLayout(new BorderLayout(0, 0));
+		
+		JSeparator separator_2 = new JSeparator();
+		p3.add(separator_2, BorderLayout.SOUTH);
+		
+		JPanel p4 = new JPanel();
+		p4.setBounds(162, 293, 176, 88);
+		getContentPane().add(p4);
+		p4.setLayout(new BorderLayout(0, 0));
+		setVisible(true);
 	}
-
 }
